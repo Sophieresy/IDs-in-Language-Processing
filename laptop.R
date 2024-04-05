d85c70347cb80e21a88a7583c3ae479b320ae631
Sophieresy <soko00002@uni-saarland.de>
2024-04-05 15:49:32 +0200

    pca now done how it was recommended

--- laptop.R
+++ laptop.R
@@ -3,6 +3,8 @@ library('corrplot')
 library('ggplot2')
 library('dplyr')
 library('factoextra')
+library('ggbiplot')
+
 # load data
 
 personality<- read.table(here::here("data", "data_L2_scalars.csv"), header=T, sep= ",")
@@ -16,35 +18,31 @@ correlation <- rcorr(as.matrix(personality))
 corrplot(correlation$r, type="lower", order="hclust",diag=0,insig = "blank",addCoef.col = 'black')
 
 ###pca nach UNI
-#pca1 <- psych::principal(personality, nfactors = 7, rotate = "none")
+personality <- scale(personality)
+pca1 <- psych::principal(personality, nfactors = 11, rotate = "none")
+summary(pca1)
+fviz_eig(pca1)
+round(pca1$values,2) # we have 6 values around 1 or bigger, so we keep 6
+eigenvalues <- pca1$values
 
-#summary(pca1)
+# Create a scree plot
+plot(eigenvalues, type = "b", main = "Scree Plot", xlab = "Component Number", ylab = "Eigenvalue", pch = 19)
+abline(h = 1, col = "red", lty = 2)  # Adds a horizontal line at eigenvalue = 1
 
-#round(pca1$values,2)
-#eigenvalues <- pca1$values
-#plot(eigenvalues, type = "b", xlab = "Principal Component", ylab = "Eigenvalue", 
- #    main = "Scree Plot", pch = 19)
-#abline(h = 1, col = "red", lty = 2) # Optional: Kaiser criterion line
-#biplot(pca1)
+#second round of pca
+pca2 <- psych::principal(personality, nfactors = 6, rotate = "varimax", scores = TRUE) 
+print(pca2)
+# openesss loads into RC1 & RC3 almoast the same
 
-#pca tutorial
-colSums(is.na(personality))# do we have NA values?
-personalityPCA <- prcomp(personality, scale =TRUE) # i need to standardize my data
-
-summary(personalityPCA)
-names(personalityPCA) #elemets
-personalityPCA$sdev #std dev 
-#std dev & mean of variables
-personalityPCA$center
-personalityPCA$scale
-
-#Principal componetnt scores
-personalityPCA$x
-
-#scree plot
-fviz_eig(personalityPCA, 
-         addlabels = TRUE)
-
-fviz_pca_biplot(personalityPCA,
-                label="var")
+#RC1 Agreeableness, Conscientiousness (openess)
+#RC2 Social Skill, Communication
+#RC3 Negative Attention switching, Positive extraversion (openess)
+#RC4 Attention to Detail, Systemizing
+#RC5 Neuroticsm
+#RC6 Imagination
+#plot
+colnames(pca2$loadings) <- c("Agreeable & Concious (open)", "-Attention switching & + extraversion (open)", "Social Skill & Communication", "Attention to Detail & Systemizing", "Imagination", "Neuroticsm")
+print(pca2)
+pca2_prcomp <- prcomp(pca2$scores)
+fviz_pca_biplot(pca2_prcomp,label="var", repel = TRUE)
 
